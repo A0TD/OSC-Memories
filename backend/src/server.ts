8a1a0de07f0ab28394eead9f2d5ec01d8c6a838a
IslamMostafaYassin <islam.mostafa.yassin@gmail.com>
2026-09-04 12:26:31 +0300

    remove terminal warning and fix package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application,Request,Response } from "express";
 import swaggerUI from "swagger-ui-express"
 import "dotenv/config";
 import connectDB from "./config/mongoDB.config";
@@ -10,6 +10,10 @@ const PORT = (process.env.PORT as string) || 3000;
 //connectDB();
 
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(specs))
+
+app.get("/", (req:Request, res:Response) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
