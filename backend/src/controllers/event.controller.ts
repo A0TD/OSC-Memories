870b5eff30700b5657c3b059eead7b51dc6f3e35
Youssef Emad <ymad88679@gmail.com>
2026-09-05 14:20:36 +0300

    Add the logic of event's functionalities in controllers

--- backend/src/controllers/event.controller.ts
+++ backend/src/controllers/event.controller.ts
@@ -5,37 +5,102 @@ import Event from "../models/event.model.js";
 import mongoose from "mongoose";
 
 export const getAllEvents = async (req: Request, res: Response) => {
-  const seasonId: any = req.params.seasonId;
-  const objectId = new mongoose.Types.ObjectId(seasonId);
-
-  const allEvents = await Event.find({ seasonId: objectId });
+  const seasonId = req.params.seasonId;
+  try{
+  const allEvents = await Event.find({ seasonId: seasonId });
 
   if (!allEvents) {
     return res.status(404).send("There are no events yet");
   }
-
   res.status(200).send(allEvents);
+ }catch(err){
+  console.error("There is an error in getAllEvents" , err);
+ }
 };
 
-export const getEvent = async (req: Request, res: Response) => {};
+export const getEvent = async (req: Request, res: Response) => {
+  const{id} = req.params;
+ try{
+    const requiredEvent = await Event.findOne({_id: id});
 
-export const deleteEvent = async (req: Request, res: Response) => {};
+    if(!requiredEvent){
+      return res.status(404).send("the required event is not found");
+    }
 
-export const updateEvent = async (req: Request, res: Response) => {};
+    //we need here to return all the media related to this event
+    const allMediaRelated:any[] = [];
+
+    res.status(200).json({
+      requiredEvent , 
+      allMediaRelated
+    });
+ }catch(err){
+  console.error("There is an error in getEvent: " , err);
+ }
+};
 
 export const createEvent = async (req: Request, res: Response) => {
-  const { name, heroImageURL, description } = req.body;
-  const seasonId: any = req.params.seasonid;
-  const objectId = new mongoose.Types.ObjectId(seasonId);
-
+    const { name, heroImageURL, description } = req.body;
+    const seasonId:any = req.params.seasonid;
+    const objectId = new mongoose.Types.ObjectId(seasonId);
+  try{
   const newEvent = await Event.create({
     name: name,
     seasonId: objectId,
     heroImage: heroImageURL,
     description: description,
   });
-
   res.status(201).json(newEvent);
+  }catch(err){
+    console.error("There is an error in createEvent" , err);
+  }
+};
+
+export const deleteEvent = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  try{
+     const requiredEvent = await Event.findOne({_id: id});
+
+      if(!requiredEvent){
+        return res.status(404).send("the required event is not found");
+      }
+
+      //we need to delete all media related to this event
+
+     const unWantedEvent = await Event.deleteOne({_id: id});
+
+     res.status(204).json({
+       message: "Event is deleted correctly"
+     });
+  }catch(err){
+    console.error("There is an error in deleteEvent" , err);
+  }
+};
+
+export const updateEvent = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const {name , heroImageURL , description} = req.body; 
+  try{
+      const updatedEvent = await Event.findByIdAndUpdate(
+        id , 
+        {
+          name:name , 
+          heroImage:heroImageURL, 
+          description: description
+        }
+      );
+
+      if(!updatedEvent){
+        return res.status(404).send("the required event is not found");
+      }
+
+     res.status(201).json({
+      message: "Event is updated" , 
+      updatedEvent
+     });
+  }catch(err){
+    console.error("There is an error in updateEvent" , err);
+  }
 };
 
 // export const getEventPhotos = async (req:Request , res:Response) => {
