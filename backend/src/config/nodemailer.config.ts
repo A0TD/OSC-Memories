6661485c8e1bf8bfc9e61f1578af080358f29881
A0TD <yousef.y.m.d6@gmail.com>
2026-09-16 19:50:32 +0300

    🔥 Fix nodemailer

--- backend/src/config/nodemailer.config.ts
+++ backend/src/config/nodemailer.config.ts
@@ -4,10 +4,11 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-});
+} as any);
 
 export default transporter;
